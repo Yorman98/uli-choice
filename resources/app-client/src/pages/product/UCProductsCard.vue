bd2ed15ad7eff62b069933e7bb71bc27aef70185
Kimberly Morales <nakary.km@gmail.com>
2023-08-12 23:20:08 -0400

    Fix on the product card

--- resources/app-client/src/pages/product/UCProductsCard.vue
+++ resources/app-client/src/pages/product/UCProductsCard.vue
@@ -3,6 +3,8 @@ import type { Ref } from 'vue'
 import { ref } from 'vue'
 import type { ProductInterface } from '@/store/types/ProductInterface'
 import ProductService from '@/services/ProductService'
+import { STORAGE_PATH } from '@/utils/constants'
+import DemoFormLayoutHorizontalForm from "@/views/pages/form-layouts/DemoFormLayoutHorizontalForm.vue";
 
 const productsList: Ref<ProductInterface[]> = ref([])
 
@@ -15,21 +17,56 @@ async function dataProducts() {
   })
 }
 
+function getProductUrl(slug: string) {
+  return `/product/${slug}`
+}
+
 onMounted(async () => {
   await dataProducts()
 })
 </script>
 
 <template>
-  <VRow class="uc-products-container">
+  <VRow class="uc-products-container ma-5">
     <VCol cols="12">
       <div
         v-for="product in productsList"
         :key="product.id"
       >
-        {{ product.image }}
-        {{ product.name }}
+        <a
+          class="cursor-pointer"
+          :href="getProductUrl(product.slug)"
+        >
+          <VCard
+            class="mx-auto"
+            max-width="350"
+          >
+            <VImg
+              :src="STORAGE_PATH + product.image"
+              :alt="product.name"
+              cover="true"
+              height="300px"
+            />
+            <VCardText>
+              <h5 class="text-h5">{{ product.name }}</h5>
+            </VCardText>
+          </VCard>
+        </a>
       </div>
     </VCol>
   </VRow>
 </template>
+
+<style scoped lang="scss">
+.uc-products-container {
+  .v-col {
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    gap: 30px 10px;
+  }
+
+  .v-img {
+    background: lightgrey;
+  }
+}
+</style>
